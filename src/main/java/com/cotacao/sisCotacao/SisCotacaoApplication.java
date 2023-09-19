8e90202c3b4fc4d4d4157f3a93f9a438814867c4
Emachirica <142347259+Emachirica@users.noreply.github.com>
2023-09-19 08:15:51 +0000

    testando: Sistema Cotacao

---
+++ src/main/java/com/cotacao/sisCotacao/SisCotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.cotacao.sisCotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisCotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisCotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotacao/sisCotacao/SisCotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotacao.sisCotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisCotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
